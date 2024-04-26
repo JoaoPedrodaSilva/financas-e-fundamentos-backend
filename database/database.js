edf8dedee8a8d87fd245a935e9929ceda1fd6f98
João Pedro da Silva <joao.pedro.da.silva@hotmail.com>
2024-04-26 14:41:36 -0300

    Add ssl connection to pg

--- database/database.js
+++ database/database.js
@@ -1,6 +1,8 @@
 const { Pool } = require("pg")
-const pool = new Pool()
+const pool = new Pool({ ssl: true })
 
 module.exports = {
-    query: (text, params) => pool.query(text, params)
+    query: (text, params, callback) => pool.query(text, params, callback)
 }
+
+
